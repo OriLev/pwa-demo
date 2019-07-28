a71b3ddff5e5bdb1e5243c99e0e0942674be9241
Ori Levitan <orilevitan2@gmail.com>
2019-07-28 14:42:36 +0300

    Updates

---
+++ precache-manifest.ba73215ef488e12b90a8674763aa240d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57da64ee32e8be0a0356b2ca67c88e5a",
+    "url": "/pwa-demo/index.html"
+  },
+  {
+    "revision": "1200a13705d0f502fb95",
+    "url": "/pwa-demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/pwa-demo/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1200a13705d0f502fb95",
+    "url": "/pwa-demo/static/js/main.6a3f9693.chunk.js"
+  },
+  {
+    "revision": "e001b62d860019334a19",
+    "url": "/pwa-demo/static/js/runtime~main.5d1df559.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
